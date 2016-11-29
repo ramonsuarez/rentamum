f25fbbecd1cf60a1a401de0e4ab0cb3aa6985d44
Dugagjin Ramani <r.dugzino@gmail.com>
2016-11-29 11:24:11 +0100

    has_many fix

--- app/models/mother_listing.rb
+++ app/models/mother_listing.rb
@@ -1,5 +1,9 @@
 class MotherListing < ApplicationRecord
   belongs_to :user
-  has_many :user_favorites, :bookings, :user_reviews, :mother_skills, :mother_listing_reviews
+  has_many :user_favorites
+  has_many :bookings
+  has_many :user_reviews
+  has_many :mother_skills
+  has_many :mother_listing_reviews
   has_many :skills, through: :mother_skill
 end
