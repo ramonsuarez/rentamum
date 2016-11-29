cb2f8cec12745f5fefafcaaccebc2962470f0ebe
Dugagjin Ramani <r.dugzino@gmail.com>
2016-11-29 12:07:56 +0100

    mothers & users x 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  MotherListing.create(
+    first_name: Faker::Name.first_name
+    last_name: Faker::Name.last_name
+    price: Faker::Commerce.price
+    location: Faker::Pokemon.location
+  )
+  User.create(
+    first_name: Faker::Name.first_name
+    last_name: Faker::Name.last_name
+    address: Faker::Address.street_address + ", " + Faker::Address.state
+    phone: Faker::PhoneNumber.phone_number
+    email: Faker::Internet.email
+  )
+end
