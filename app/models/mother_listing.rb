58682451cfc9852a3ce51507c6534221e4f1922c
Dugagjin Ramani <r.dugzino@gmail.com>
2016-11-29 10:50:21 +0100

    has_many added

--- app/models/mother_listing.rb
+++ app/models/mother_listing.rb
@@ -1,3 +1,5 @@
 class MotherListing < ApplicationRecord
   belongs_to :user
+  has_many :user_favorites, :bookings, :user_reviews, :mother_skills, :mother_listing_reviews
+  has_many :skills, through: :mother_skill
 end
