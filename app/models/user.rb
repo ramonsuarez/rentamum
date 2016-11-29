ef3562f8622121b2e4950e9cc431e35ec4e45621
Ramon Suarez <ramon@betacowork.com>
2016-11-29 13:42:01 +0100

    Associations User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  has_many :user_reviews
+  has_many :user_favorites
+  has_many :bookings
+  has_many :mother_listings # Could be renting out one or more moms
+  has_many :mother_listing_reviews
+  # Shortcut to be able to refference mother_listing directly without going through booking
+  has_many :book_moms, through: :bookings, :source => :mother_listing
+  # Shortcut to be able to refference mother_listing directly without going through user_favorite
+  has_many :favorite_moms, through: :user_favorites, :source => :mother_listing
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
