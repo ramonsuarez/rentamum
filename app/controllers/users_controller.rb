a5fd1db7638aead9cc8a49fbb5e7f123a698dfce
Ramon Suarez <ramon@betacowork.com>
2016-11-29 15:49:35 +0100

    Add user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find(user_params)
+    # TODO Protect params
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id)
   end
 end
