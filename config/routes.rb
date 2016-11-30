f094158dc1735f184465598301068c34dbf4b5ba
luciana diehl <lu.diehl@gmail.com>
2016-11-30 10:51:10 -0500

    added image and description to mum listing show

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  #resources :users, only: :show
+  resources :users, only: :show
   resources :mother_listings do
     resources :bookings, only: [:show, :new, :create, :destroy]
   end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  get "user/:id", to: 'users#show'
+  # get "user/:id", to: 'users#show'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
