f88c6101998d960b96071e35b41485aa9d3ba24a
Dugagjin Ramani <r.dugzino@gmail.com>
2016-12-01 14:06:47 +0100

    sign up fix +first_name +last_name

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'pages#home'
   resources :users, only: :show
 
