f714c6e8cca8943f9f0bf1eac81c791faa7ecf17
luciana diehl <lu.diehl@gmail.com>
2016-12-01 05:03:40 -0500

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :users, only: :show
   resources :mother_listings do
     resources :bookings, only: [:show, :new, :create, :destroy]
   end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  # get "user/:id", to: 'users#show'
   root to: 'pages#home'
+  resources :users, only: :show
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
